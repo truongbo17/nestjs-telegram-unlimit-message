48586325f9e47c9d4cde4a9ad92264bfde1a7842
truongbo <truongnq017@gmail.com>
2025-01-09 15:32:38 +0700

    feature: update telegram request

--- src/telegram.service.ts
+++ src/telegram.service.ts
@@ -1,14 +1,15 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { Observable } from 'rxjs';
+import { catchError, map, Observable } from 'rxjs';
 import {
   TelegramMessage,
   TelegramResponse,
   TelegramSendMessageParams,
   TelegramUser,
 } from './interfaces/telegram.interface';
-import { AxiosRequestConfig, AxiosResponse } from 'axios';
+import { AxiosError, AxiosRequestConfig } from 'axios';
 import { HttpService } from '@nestjs/axios';
+import { TelegramRequestException } from './exceptions/telegram-request.exception';
 import { TELEGRAM_MODULE_PROVIDER } from './constants/telegram.constant';
 import { TelegramModuleOptions } from './interfaces/telegram-module-option.interface';
 import { TelegramNotBotKeyException } from './exceptions/telegram-not-bot-key.exception';
@@ -26,16 +27,16 @@ export class TelegramService {
     private readonly configService: ConfigService
   ) {}
 
-  public getMe(): Observable<AxiosResponse<TelegramResponse<TelegramUser>>> {
-    return this.handleRequest<TelegramResponse<TelegramUser>>(
+  public getMe(): Observable<TelegramUser> {
+    return this.handleRequest<TelegramUser>(
       this.configService.get('telegram.getMe')
     );
   }
 
   public sendMessage(
     data: TelegramSendMessageParams
-  ): Observable<AxiosResponse<TelegramResponse<TelegramMessage>>> {
-    return this.handleRequest<TelegramResponse<TelegramMessage>>(
+  ): Observable<TelegramMessage> {
+    return this.handleRequest<TelegramMessage>(
       this.configService.get('telegram.sendMessage'),
       data
     );
@@ -63,9 +64,24 @@ export class TelegramService {
     endpoint: string,
     data: object = {},
     axiosOptions?: AxiosRequestConfig
-  ): Observable<AxiosResponse<T>> {
+  ): Observable<T> {
     const rootUrl: string = this.configService.get<string>('telegram.url');
     const apiUrl: string = `${rootUrl}${this.getBotKey()}${endpoint}`;
-    return this.http.post(apiUrl, data, axiosOptions);
+    return this.http.post<TelegramResponse<T>>(apiUrl, data, axiosOptions).pipe(
+      map((res: any) => {
+        if (!res.data.ok) {
+          throw new TelegramRequestException(res.data.description);
+        }
+        return res.data.result;
+      }),
+      catchError((error: Error) => {
+        if (error instanceof AxiosError) {
+          throw new TelegramRequestException(
+            error?.response?.data?.description || error.message
+          );
+        }
+        throw new TelegramRequestException(error.message);
+      })
+    );
   }
 }
